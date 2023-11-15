ebe7402233a958ccaf16e01423c17d51cac98341
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-15 15:15:10 +0100

    initial scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { store } from './data/store';
+export default {
+  name:'App',
+  data () {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
