f7e2fd37687b1728e1f57e38ecba8cc6022c89c3
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-15 17:52:00 +0100

    add getmovies() and getSeries()

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,76 @@
 <template>
-  <div class="bg-black w-100 nav d-flex align-items-center py-3">
-    <div class="col-2 d-flex justify-content-center">
-      <h1 class="text-uppercase">
-        boolflix
-      </h1>
-    </div>
-    <div class="col-8">
-      <div class="input-group">
-        <span class="input-group-text border-0 bg-white" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"
-            style="color: #828487;"></i></span>
-        <input type="text" class="form-control border-0" placeholder="Cerca un titolo" aria-label="Username"
-          aria-describedby="basic-addon1">
-        <span class="input-group-text border-0 bg-search text-white" id="basic-addon2">Cerca</span>
+  <div class="app d-flex flex-column">
+    <header class="bg-black w-100">
+      <div class=" nav d-flex align-items-center py-3">
+        <div class="col-2 d-flex justify-content-center">
+          <h1 class="text-uppercase">
+            boolflix
+          </h1>
+        </div>
+        <div class="col-8">
+          <div class="input-group">
+            <span class="input-group-text border-0 bg-white" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"
+                style="color: #828487;"></i></span>
+            <input type="text" class="form-control border-0" placeholder="Cerca un titolo" aria-label="Username"
+              aria-describedby="basic-addon1">
+            <span class="input-group-text border-0 bg-search text-white" id="basic-addon2">Cerca</span>
+          </div>
+        </div>
+        <div class="col-2 d-flex justify-content-center">
+          <a href="#">
+            <i class="fa-solid fa-user" style="color: #828487;"></i>
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-gear" style="color: #828487;"></i>
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-bell" style="color: #828487;"></i>
+          </a>
+        </div>
       </div>
-    </div>
-    <div class="col-2 d-flex justify-content-center">
-      <a href="#">
-        <i class="fa-solid fa-user" style="color: #828487;"></i>
-      </a>
-      <a href="#">
-        <i class="fa-solid fa-gear" style="color: #828487;"></i>
-      </a>
-      <a href="#">
-        <i class="fa-solid fa-bell" style="color: #828487;"></i>
-      </a>
-    </div>
+    </header>
+    <main class="flex-grow-1 overflow-auto">
+
+    </main>
   </div>
 </template>
 
 <script>
 import { store } from './data/store';
+import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return {
-      store
+      store,
     }
+  },
+  methods: {
+    getMovies() {
+      return axios.get(store.apiUrl + store.endPoint.movies, { params: { api_key: store.params.apiKey, query: store.params.query } })
+    },
+    getSeries() {
+      return axios.get(store.apiUrl + store.endPoint.series, { params: { api_key: store.params.apiKey, query: store.params.query } })
+    },
+  },
+  created() {
+    Promise.all([this.getMovies(),this.getSeries()]).then((results)=>{
+      const movies = results[0].data.results;
+      store.movieList = movies
+      const series = results[1].data.results;
+      store.seriesList = series
+      console.log(movies)
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.nav {
+.app {
+  height: 100vh;
+}
 
+.nav {
   h1 {
     color: #e6111b;
   }
@@ -56,6 +84,10 @@ export default {
 .bg-search {
   background-color: #e6111b;
 
+}
+
+main {
+  background-color: #828487;
 }
 
 .bg-search:hover {
--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint: {
+        movies: 'search/movie',
+        series: 'search/tv'
+    },
+    params: {
+        apiKey: 'ce9c620fce570ae78b7e22a8e754e124',
+        query: 'a'
+    },
+    movieList: [],
+    seriesList: [],
 
 });
